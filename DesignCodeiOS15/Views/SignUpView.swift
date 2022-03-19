aed07bb2a1a557985639d82aa54ab19314b207a7
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-03-19 23:49:35 +0300

    Custom Input Style

--- DesignCodeiOS15/Views/SignUpView.swift
+++ DesignCodeiOS15/Views/SignUpView.swift
@@ -8,54 +8,66 @@
 import SwiftUI
 
 struct SignUpView: View {
-    var body: some View {
-      VStack(alignment: .leading, spacing: 16) {
-        Text("Sign up")
-          .font(.largeTitle).bold()
-        Text("Access 120+ hours of courses, tutorials and livestreams")
-          .font(.headline)
-        Button {} label: {
-          Text("Create an account")
-            .frame(maxWidth: .infinity)
-        }
+  @State var email = ""
+  @State var password = ""
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 16) {
+      Text("Sign up")
+        .font(.largeTitle).bold()
+      Text("Access 120+ hours of courses, tutorials and livestreams")
         .font(.headline)
-        .blendMode(.overlay)
-        .buttonStyle(.angular)
-        .tint(.accentColor)
-        .controlSize(.large)
+      TextField("Email", text: $email)
+        .inputStyle(icon: "mail")
+        .textContentType(.emailAddress)
+        .keyboardType(.emailAddress)
+        .autocapitalization(.none)
+        .disableAutocorrection(true)
+      SecureField("Password", text: $password)
+        .inputStyle(icon: "lock")
+        .textContentType(.password)
+      Button {} label: {
+        Text("Create an account")
+          .frame(maxWidth: .infinity)
+      }
+      .font(.headline)
+      .blendMode(.overlay)
+      .buttonStyle(.angular)
+      .tint(.accentColor)
+      .controlSize(.large)
+      
+      Group {
+        Text("By clicking on")
+        + Text("__Create an account__").foregroundColor(.primary.opacity(0.7))
+        + Text(", you agree to our **Terms of service** and **[Privacy policy](https://developer.apple.com)**")
         
-        Group {
-          Text("By clicking on")
-          + Text("__Create an account__").foregroundColor(.primary.opacity(0.7))
-          + Text(", you agree to our **Terms of service** and **[Privacy policy](https://developer.apple.com)**")
-          
-          Divider()
-          
-          HStack {
-            Text("Already have an account?")
-            Button {} label: {
-              Text("**Sign in**")
-            }
+        Divider()
+        
+        HStack {
+          Text("Already have an account?")
+          Button {} label: {
+            Text("**Sign in**")
           }
         }
-        .font(.footnote)
-        .foregroundColor(.secondary)
-        .accentColor(.secondary)
       }
-      .padding(20)
-      .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
-      .strokeStyle(cornerRadius: 30)
-      .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
-      .padding(20)
-      .background(
-        Image("Blob 1").offset(x: 200, y: -100)
-      )
+      .font(.footnote)
+      .foregroundColor(.secondary)
+      .accentColor(.secondary)
     }
+    .padding(20)
+    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+    .strokeStyle(cornerRadius: 30)
+    .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
+    .padding(20)
+    .background(
+      Image("Blob 1").offset(x: 200, y: -100)
+    )
+  }
 }
 
 struct SignUpView_Previews: PreviewProvider {
-    static var previews: some View {
-      SignUpView()
-        .preferredColorScheme(.light)
-    }
+  static var previews: some View {
+    SignUpView()
+      .preferredColorScheme(.light)
+  }
 }
