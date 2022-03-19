8b542ab240de96b93732fb2fc38581caed393428
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-03-19 22:32:52 +0300

    Custom Modal and Button Style

--- DesignCodeiOS15/Navigation/ContentView.swift
+++ DesignCodeiOS15/Navigation/ContentView.swift
@@ -8,41 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("selectedTab") var selectedTab: Tab = .home
-    @EnvironmentObject var model: Model
-    
-    var body: some View {
-        ZStack(alignment: .bottom) {
-            switch selectedTab {
-            case .home:
-                HomeView()
-            case .explore:
-                AccountView()
-            case .notifications:
-                AccountView()
-            case .library:
-                AccountView()
+  @AppStorage("selectedTab") var selectedTab: Tab = .home
+  @AppStorage("showModal") var showModal = false
+  @EnvironmentObject var model: Model
+  
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      switch selectedTab {
+      case .home:
+        HomeView()
+      case .explore:
+        AccountView()
+      case .notifications:
+        AccountView()
+      case .library:
+        AccountView()
+      }
+      
+      TabBar()
+        .offset(y: model.showDeteil ? 200 : 0)
+      
+      if showModal {
+        ZStack {
+          Color.clear.background(.regularMaterial)
+            .ignoresSafeArea()
+          
+          SignUpView()
+          
+          Button {
+            withAnimation {
+              showModal = false
             }
-            
-            TabBar()
-                .offset(y: model.showDeteil ? 200 : 0)
+          } label: {
+            Image(systemName: "xmark")
+              .font(.body.weight(.bold))
+              .foregroundColor(.secondary)
+              .padding(8)
+              .background(.ultraThinMaterial, in: Circle())
+          }
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+          .padding(20)
         }
-        .safeAreaInset(edge: .bottom, spacing: 0) {
-            Color.clear.frame(height: 44)
-        }
+        .zIndex(1)
+      }
     }
+    .safeAreaInset(edge: .bottom, spacing: 0) {
+      Color.clear.frame(height: 44)
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            ContentView()
-                .preferredColorScheme(.light)
-                .previewDevice("iPhone 13 Pro Max")
-            ContentView()
-                .preferredColorScheme(.dark)
-                .previewDevice("iPhone 13 Pro Max")
-        }
-        .environmentObject(Model())
+  static var previews: some View {
+    Group {
+      ContentView()
+        .preferredColorScheme(.light)
+        .previewDevice("iPhone 13 Pro Max")
+      ContentView()
+        .preferredColorScheme(.dark)
+        .previewDevice("iPhone 13 Pro Max")
     }
+    .environmentObject(Model())
+  }
 }
--- DesignCodeiOS15/Navigation/NavigationBar.swift
+++ DesignCodeiOS15/Navigation/NavigationBar.swift
@@ -12,6 +12,7 @@ struct NavigationBar: View {
     @Binding var hasScrolled: Bool
     @State var showSearch = false
     @State var showAccount = false
+    @AppStorage("showModal") var showModal = false
     
     var body: some View {
         ZStack {
@@ -41,7 +42,9 @@ struct NavigationBar: View {
                 }
                 
                 Button {
-                    showAccount = true
+                  withAnimation {
+                    showModal = true
+                  }
                 } label: {
                     Image("Avatar Default")
                         .resizable()
