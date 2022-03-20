e52acc5ba3ca2594728e7835b98fcedc844b8ff3
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-03-21 01:02:55 +0300

    Dynamic Type Sizes

---
+++ DesignCodeiOS15/Components/Items/FeaturedItem.swift
@@ -0,0 +1,59 @@
+//
+//  FeaturedItem.swift
+//  DesignCodeiOS15
+//
+//  Created by Татьяна Мальчик on 21.02.2022.
+//
+
+import SwiftUI
+
+struct FeaturedItem: View {
+  var course: Course = courses[0]
+  @Environment(\.sizeCategory) var sizeCategory
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 8.0) {
+      Spacer()
+      Image(course.logo)
+        .resizable(resizingMode: .stretch)
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 26.0, height: 26.0)
+        .cornerRadius(10)
+        .padding(9)
+        .background(Color(UIColor.systemBackground).opacity(0.1), in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+        .strokeStyle(cornerRadius: 16)
+      Text(course.title)
+        .font(.largeTitle)
+        .fontWeight(.bold)
+        .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+        .lineLimit(1)
+        .font(.title)
+        .dynamicTypeSize(.large)
+      Text(course.subtitle.uppercased())
+        .font(.footnote)
+        .fontWeight(.semibold)
+        .foregroundStyle(.secondary)
+      Text(course.text)
+        .font(.footnote)
+        .multilineTextAlignment(.leading)
+        .lineLimit(sizeCategory > .large ? 1 : 2)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .foregroundColor(.secondary)
+    }
+    .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+    .padding(.vertical, 20)
+    .frame(height: 350.0)
+    .background(.ultraThinMaterial)
+    .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+    //        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+    //        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+    .strokeStyle()
+    .padding(.horizontal, 20)
+  }
+}
+
+struct FeaturedItem_Previews: PreviewProvider {
+  static var previews: some View {
+    FeaturedItem()
+  }
+}
