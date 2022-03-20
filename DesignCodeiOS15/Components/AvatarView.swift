4138d6bfbe368b1a496a3e68294d386dccefffc8
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-03-20 18:33:07 +0300

    AsyncImage

---
+++ DesignCodeiOS15/Components/AvatarView.swift
@@ -0,0 +1,38 @@
+//
+//  AvatarView.swift
+//  DesignCodeiOS15
+//
+//  Created by Татьяна Мальчик on 20.03.2022.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+  var body: some View {
+    AsyncImage(url: URL(string: "https://mobimg.b-cdn.net/v3/fetch/b8/b89e89af4f740c3e4e9fbd535cf41e4f.jpeg"), transaction: Transaction(animation: .easeOut)) { phase in
+      switch phase {
+      case .success(let image):
+        image.resizable()
+          .transition(.scale(scale: 0.5, anchor: .center))
+      case .empty:
+        ProgressView()
+      case .failure(_):
+        Color.gray
+      @unknown default:
+        EmptyView()
+      }
+
+    }
+    .frame(width: 26, height: 26)
+    .cornerRadius(10)
+    .padding(8)
+    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+    .strokeStyle(cornerRadius: 18)
+  }
+}
+
+struct AvatarView_Previews: PreviewProvider {
+  static var previews: some View {
+    AvatarView()
+  }
+}
