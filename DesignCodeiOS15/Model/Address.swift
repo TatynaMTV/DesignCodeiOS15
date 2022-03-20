59de5d7aa65a239803ea05952d7f8156a26b20be
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-03-20 19:23:03 +0300

    Receive JSON data _ Async / Await

---
+++ DesignCodeiOS15/Model/Address.swift
@@ -0,0 +1,14 @@
+//
+//  Address.swift
+//  DesignCodeiOS15
+//
+//  Created by Татьяна Мальчик on 20.03.2022.
+//
+
+import SwiftUI
+
+struct Address: Identifiable, Decodable {
+  var id: Int
+  var country: String
+  
+}
