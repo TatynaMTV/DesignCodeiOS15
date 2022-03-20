cba5b916abc860186a39571de6885fa720c23cf6
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-03-20 20:07:47 +0300

    Performance and Lite Mode

--- DesignCodeiOS15/Views/AccountView.swift
+++ DesignCodeiOS15/Views/AccountView.swift
@@ -13,6 +13,7 @@ struct AccountView: View {
   @State var address: Address = Address(id: 1, country: "Canada")
   @Environment(\.dismiss) var dismiss
   @AppStorage("isLogged") var isLogged = false
+  @AppStorage("isLiteMode") var isLiteMode = true
   @ObservedObject var coinModel = CoinModel()
   
   func fetchAddress() async {
@@ -30,6 +31,14 @@ struct AccountView: View {
       List {
         profile
         menu
+        
+        Section {
+          Toggle(isOn: $isLiteMode) {
+            Label("Lite Mode", systemImage: isLiteMode ? "tortoise" : "hare")
+          }
+        }
+        .accentColor(.primary)
+        
         links
         coins
         
